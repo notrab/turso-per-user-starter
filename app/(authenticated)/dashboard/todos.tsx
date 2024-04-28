73d40ff87af13d9a2b912b3d122f9f9f81cae525
Jamie Barton <jamie@notrab.dev>
2024-04-28 16:57:30 +0100

    da todoitem

--- app/(authenticated)/dashboard/todos.tsx
+++ app/(authenticated)/dashboard/todos.tsx
@@ -1,6 +1,7 @@
 import { getDatabaseClient } from "@/app/utils";
 
 import { Todo } from "./todo";
+import { type TodoItem } from "./actions";
 
 export async function Todos() {
   const client = await getDatabaseClient();
@@ -9,7 +10,9 @@ export async function Todos() {
     return <p>No such table `todos`</p>;
   }
 
-  const { rows } = await client.execute("SELECT * FROM todos");
+  const result = await client.execute("SELECT * FROM todos");
+
+  const rows = result.rows as unknown as Array<TodoItem>;
 
   if (!rows || rows.length === 0) return null;
 
